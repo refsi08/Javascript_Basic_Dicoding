8b4ff93c885402f477d0be5006093b1673098471
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-05-18 14:53:52 +0700

    revisi function

--- function.js
+++ function.js
@@ -21,6 +21,19 @@ function myParamFunc(param1, param2) {
   console.log("param 2 adalah : ", param2);
 }
 
+// contoh lain Function parameter
+const user = {
+  id: 24,
+  displayName: "kren",
+  fullName: "kylo Ren",
+};
+
+function introduce({ displayName, fullName }) {
+  console.log(`${displayName} is ${fullName}`);
+}
+
+introduce(user);
+
 myParamFunc("parameter 1", "parameter 2");
 
 // Return Keyword
@@ -32,7 +45,25 @@ console.log("Luas Segitiga : ", square(3, 4));
 
 // Arrow function
 const myArrowFunc = () => {
-    return 'Ini adalah arrow function saya';
+  return "Ini adalah arrow function saya";
 };
 
 console.log(myArrowFunc());
+
+// 4. Default Parameter = memberikan nilai di awal pada parameter
+function segitigaSamakaki(sisi1, sisi2 = 2) {
+  return sisi1 * sisi2;
+}
+
+console.log(segitigaSamakaki(2)); // pada parameter 2 telah diberikan nilai default pada parameter nya
+
+// 5. rest parameter
+function sum(...numbers) {
+  let result = 0;
+  for (let number of numbers) {
+    result += number;
+  }
+  return result;
+}
+
+console.log(sum(1, 2, 3, 4, 5));
