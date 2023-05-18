bd54a84b496745fd00e8cc086932ef008ea391b2
RefDev <62316890+RefDev-00@users.noreply.github.com>
2023-05-18 22:37:05 +0700

    inheritance

---
+++ OOP/inheritance.js
@@ -0,0 +1,43 @@
+/*
+    keyword Extends
+*/
+
+// superclass
+class MailService {
+  constructor(sender) {
+    this.sender = sender;
+  }
+
+  sendMessage(message, receiver) {
+    console.log(`${this.sender} sent ${message} to ${receiver}`);
+  }
+}
+
+// Subclass
+class WhatsAppService extends MailService {
+  sendBroadcastMessage(message, receivers) {
+    for (const receiver of receivers) {
+      this.sendMessage(message, receiver);
+    }
+  }
+}
+
+// Subclass
+class EmailService extends MailService {
+  sendDelayMessage(message, receiver, delay) {
+    setTimeout(() => {
+      this.sendMessage(message, receiver);
+    }, delay);
+  }
+}
+
+const whatsapp = new WhatsAppService('Refsi');
+const email = new EmailService('dimas@dicoding.com');
+ 
+whatsapp.sendMessage('Hello', '+6289876543210');
+whatsapp.sendBroadcastMessage('Hello', ['+6289876543210', '+6282234567890']);
+// whatsapp.sendDelayedMessage(); // Error
+ 
+email.sendMessage('Hello', 'john@doe.com');
+email.sendDelayedMessage('Hello', 'john@doe.com', 3000);
+// email.sendBroadcastMessage(); // Error
